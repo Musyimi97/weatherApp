cd2e3255c7c0c76fd9c06728c5f039d460839f85
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-16 11:47:34 +0300

    alter coty weather

--- weather/views.py
+++ weather/views.py
@@ -4,9 +4,8 @@ import requests
 # Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=8db91afbbebc6c6b2fb7ac46b1803961'
-    city= 'Nairobi'
+    city= 'Las Vegas'
     r=requests.get(url.format(city)).json()
-    print(r.text)
 
     city_weather={
         'city':city,
@@ -16,3 +15,5 @@ def index(request):
     }
 
     return render(request, 'weather/weather.html')
+
+    print(city_weather)
