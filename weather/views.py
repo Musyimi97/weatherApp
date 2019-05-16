16c6639e285c9b24ef227dceab67514835b5fd63
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-16 10:51:03 +0300

    serious mess

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+import pip._vendor.requests.api
 
 # Create your views here.
 def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=8db91afbbebc6c6b2fb7ac46b1803961'
+    city= 'Nairobi'
+    r=requests.get(url.format(city))
+    print(r.text)
+
     return render(request, 'weather/weather.html')
