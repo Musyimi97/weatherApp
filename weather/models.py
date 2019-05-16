59100a7546a946a3eac4565ff5fd807f2e5bb331
Musyimi97 <collinsmusyimi.cm@gmail.com>
2019-05-16 11:53:57 +0300

    models craeted

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Cities(models.Model):
+    name= models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+    class meta:
+        verbose_name_plural='cities'
+
